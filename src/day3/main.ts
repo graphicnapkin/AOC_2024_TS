622d5bad3041f210fa08e9cef978033e056139d8
Jon Carter <graphicnapkin@me.com>
2024-12-21 23:39:16 -0500

    completed day 3

---
+++ src/day3/main.ts
@@ -0,0 +1,30 @@
+import { readTextFile } from "../utils";
+
+const main = async () => {
+  const test = false;
+  let rawInput = await readTextFile(
+    __dirname + (test ? "/testInput.txt" : "/input.txt")
+  );
+  let total = 0;
+  //handle conditionals
+  rawInput = rawInput
+    .split("do()")
+    .map((r) => r.split("don't()")[0])
+    .join();
+
+  const multiplyRegex = /mul\(\d{1,3},\d{1,3}\)/g;
+  const matches = rawInput.matchAll(multiplyRegex);
+
+  for (const match of matches) {
+    let instruction = match[0];
+    const [num1, num2] = instruction
+      .replace(/mul\(|\)/, "")
+      .split(",")
+      .map((n) => parseInt(n));
+
+    total += num1 * num2;
+  }
+  console.log({ total });
+};
+
+export default main;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import day1 from './day1/main'
 import day2 from './day2/main'
+import day3 from './day3/main'
 
 // day1()
-day2()
+// day2()
+day3()
